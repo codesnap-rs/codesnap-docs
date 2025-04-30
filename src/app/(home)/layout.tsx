4384fb4ffdea61268fb05b71f06cb2a8e2712dfd
Mist <mist.zzh@gmail.com>
2025-05-01 01:26:38 +0800

    [Feat] add logo & update page layout width

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,7 +1,20 @@
-import type { ReactNode } from 'react';
-import { HomeLayout } from 'fumadocs-ui/layouts/home';
-import { baseOptions } from '@/app/layout.config';
+import type { ReactNode } from "react";
+import { HomeLayout } from "fumadocs-ui/layouts/home";
+import { baseOptions } from "@/app/layout.config";
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <HomeLayout {...baseOptions}>{children}</HomeLayout>;
+  return (
+    <HomeLayout
+      {...baseOptions}
+      links={[
+        {
+          text: "Documentation",
+          url: "/docs/cli",
+          active: "nested-url",
+        },
+      ]}
+    >
+      {children}
+    </HomeLayout>
+  );
 }
