f5d091cd1794056b1d7ba5bb201627297288abc9
Mist <mist.zzh@gmail.com>
2025-05-07 18:46:14 +0800

    [Fix] hydration error cause client dismatched initial value

--- src/components/version-chip.tsx
+++ src/components/version-chip.tsx
@@ -7,5 +7,5 @@ import { Chip } from "@heroui/react";
 export const VersionChip = ({ className }: WithStyle<{}>) => {
   const { version } = useVersion();
 
-  return <Chip className={className}>Version:{version}</Chip>;
+  return <Chip className={className}>{!version ? "Loading..." : version}</Chip>;
 };
