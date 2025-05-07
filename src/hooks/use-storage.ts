5740afb474a38e49ef74a54f9bef0470618e4973
Mist <mist.zzh@gmail.com>
2025-05-07 18:46:14 +0800

    [Fix] hydration error cause client dismatched initial value

--- src/hooks/use-storage.ts
+++ src/hooks/use-storage.ts
@@ -1,21 +1,17 @@
 import { useCallback, useEffect, useState } from "react";
 
 export const useStorage = <T>(key: string, initialValue: T) => {
-  const [value, setValue] = useState(() => {
-    if (typeof window !== "undefined") {
-      const storedValue = window.localStorage.getItem(key);
-      return storedValue ? JSON.parse(storedValue) : initialValue;
-    }
-
-    return initialValue;
-  });
+  const [value, setValue] = useState(initialValue);
 
   useEffect(() => {
     const value = window.localStorage.getItem(key);
 
     if (!value) {
       window.localStorage.setItem(key, JSON.stringify(initialValue));
+      return;
     }
+
+    setValue(JSON.parse(value));
   }, [initialValue, key]);
 
   const setStoredValue = useCallback((newValue: T) => {
