60f083559d95fbee83da4f6221b373bd79b6d59d
inex <1769031928@qq.com>
2025-05-08 21:19:07 +0800

    [Fix] update style

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -1,6 +1,6 @@
 export const Heading = () => (
   <>
-    <h1 className="flex mb-1.5 text-balance text-2xl font-semibold tracking-tight sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl [&>span]:dark:from-muted-foreground [&>span]:dark:to-foreground [&>span]:dark:bg-gradient-to-t [&>span]:dark:to-40% [&>span]:dark:bg-clip-text [&>span]:dark:text-transparent">
+    <h1 className="flex mb-1.5 text-left text-2xl font-semibold tracking-tight sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl [&>span]:dark:from-muted-foreground [&>span]:dark:to-foreground [&>span]:dark:bg-gradient-to-t [&>span]:dark:to-40% [&>span]:dark:bg-clip-text [&>span]:dark:text-transparent">
       The
       <div className="flex ml-3 mb-1.5 text-2xl font-semibold tracking-tight sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl [&>span]:dark:from-muted-foreground [&>span]:dark:to-foreground [&>span]:dark:bg-gradient-to-t [&>span]:dark:to-40% [&>span]:dark:bg-clip-text [&>span]:dark:text-transparent from-[#FF1CF7] to-[#b249f8] bg-clip-text text-transparent bg-gradient-to-b">
         Most Beautiful,
@@ -15,7 +15,7 @@ export const Heading = () => (
         Customizable,
       </span>
     </h2>
-    <h1 className="flex text-balance text-2xl font-semibold tracking-tight sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl [&>span]:dark:from-muted-foreground [&>span]:dark:to-foreground [&>span]:dark:bg-gradient-to-t [&>span]:dark:to-40% [&>span]:dark:bg-clip-text [&>span]:dark:text-transparent">
+    <h1 className="flex text-left text-2xl font-semibold tracking-tight sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl [&>span]:dark:from-muted-foreground [&>span]:dark:to-foreground [&>span]:dark:bg-gradient-to-t [&>span]:dark:to-40% [&>span]:dark:bg-clip-text [&>span]:dark:text-transparent">
       code snapshot tool in Pure Rust
     </h1>
   </>
