f035bc3d4abfa03343058c38edafc210276111bc
inex <1769031928@qq.com>
2025-05-08 21:28:18 +0800

    [Fix] update style

--- src/components/get-started.tsx
+++ src/components/get-started.tsx
@@ -5,12 +5,12 @@ import { Button, Code } from "@heroui/react";
 
 export const GetStarted = ({ className }: WithStyle<{}>) => {
   return (
-    <div className={`flex items-center ${className}`}>
+    <div className={`flex ${className}`}>
       <Button className="mr-4 rounded-full" color="primary">
         Get started
       </Button>
       <Code
-        className="h-10 rounded-full max-w-fit w-full overflow-x-auto overflow-y-hidden flex items-center px-6"
+        className="h-10 rounded-full sm:max-w-fit w-full overflow-x-auto overflow-y-hidden flex items-center px-6"
         size="md"
       >
         $ codesnap -f ./main.rs -o clipboard
